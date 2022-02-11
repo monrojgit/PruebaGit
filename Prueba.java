cbbeadd8d1a8f9edb6ee5a9af12e38a135758064
Monica Rojas <monroj@gmail.com>
2022-02-11 18:47:05 -0500

    mi primer commit

---
+++ Prueba.java
@@ -0,0 +1,28 @@
+
+public Prueba{
+
+    
+public static void main(String[] args) {
+    
+
+    String cadena ="CB0";
+    String inversa="";
+
+    for (int i = 0; i < source.length(); ++i) {
+        
+        if (i=0 && source.charAt(i) != '0') {
+           
+        }
+        else
+            inversa += resultado.charAt(i);
+
+
+    }
+  
+    System.out.println("Resultado: "+ inversa)
+
+
+}
+
+}
+ 
